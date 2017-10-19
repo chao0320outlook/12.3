929dccbc9171c54b9e809e0418d159867ced9c65
Chao Chao <chao0320@outlook.com>
2017-10-19 17:11:00 +0800

    程序初步完工

--- 12.3/12.1.h
+++ 12.3/12.1.h
@@ -4,19 +4,35 @@
 #ifndef strngbad_h_
 #define strngbad_h_
 
+using namespace std;
+
 class String                                          //自定义string类
 {
 private:
 	char* str;                                        //存储字符串内容
-	int len;                                          //长度
+	int len;                                          //str长度
 	static int mun_strings;                           //数组长度
+	static const int CINLIM = 80;                     //输入最大限制
 public:
 	String(const char*s);                            //构造函数
-	String(const String &st);                      //构造函数
+	String(const String &st);                        //构造函数
 	String();                                        //默认构造函数
 	~String();                                       //析构函数
+	int length()const { return len; };               //返回长度  （）后加const 保证调用对象不会被修改
+	
+	String & operator =(const String &st);           //赋值语句，从String对象到String对象
+	String & operator =(const char*s);               //赋值语句，从*char到String对象
+	char & operator[](int i);                        //返回字符串的某个字符，可用于字符修改
+	const char & operator [](int i)const;            //返回字符串的某个字符，不可以修改字符
+	
+	friend bool operator <(const String &st1, const String &st2);
+	friend bool operator >(const String &st1, const String &st2);
+	friend bool operator ==(const String &st1, const String &st2);
 
 	friend std::ostream & operator <<(std::ostream &os, const String &st);       //重载 << 运算符
+	friend std::istream & operator >>(std::istream &is, String &st);             //重载 << 运算符
+
+	static int HowMany();                            //所创建对象数目
 };
 
 
--- 12.3/12.2.cpp
+++ 12.3/12.2.cpp
@@ -5,47 +5,105 @@
 
 using std::cout;
 
-int String::mun_strings = 0;                                   //数组内成员数
+int String::mun_strings = 0;                                    //所创建对象数目
 
 String::String(const char*s)                                    //构造函数
 {
-	len = std::strlen(s);
+	len = std::strlen(s);                                       //计算 s 长度
 	str = new char[len + 1];
-	strcpy_s(str,len+1, s);
-	mun_strings++;
-	cout << mun_strings << ": \"" << str << "\" object created\n";
+	strcpy_s(str, len + 1, s);                                  //将s复制到str
+	mun_strings++;                                              //对象数目+1
 }
 
 
-String::String(const String &st)                              //复制构造函数
+String::String(const String &st)                                //复制构造函数
 {
 	mun_strings++;
 	len = st.len;
 	str = new char[len + 1];
 	strcpy_s(str, len + 1, st.str);
-	mun_strings++;
-	cout << mun_strings << ": \"" << str << "\" object created\n";
 }
 
 String::String()                                                //默认构造函数
 {
 	len = 4;
 	str = new char[4];
-	strcpy_s(str,len+1, "C++");
+	str[0] = '\0';                                              //构造长度为4的空字符串
 	mun_strings++;
-	cout << mun_strings << ": \"" << str << "\" default object created\n";
 }
 
 String::~String()                                               //析构函数
 {
-	cout <<"\""<< str <<"\"  object seleted, ";
 	--mun_strings;
-	cout << mun_strings << " left\n";
 	delete[]str;
 }
 
-std::ostream & operator <<(std::ostream & os, const String &st)
+String & String::operator=(const String &st)                   //赋值语句，从String对象到String对象
+{
+	if (this == &st)
+		return *this;
+	delete[]str;
+	len = st.len;
+	str = new char[len + 1];
+	strcpy_s(str, len + 1, st.str);
+	return *this;
+}
+
+String & String::operator=(const char * s)                    //赋值语句，从*char到String对象
+{
+	delete[]str;
+	len = std::strlen(s);
+	str = new char[len + 1];
+	strcpy_s(str, len + 1, s);
+	return *this;
+}
+
+char & String::operator[](int i)                              //返回字符串的某个字符，可用于字符修改
+{
+	return str[i];
+}
+
+const char & String::operator[](int i) const                  //返回字符串的某个字符，不可以修改字符
+
+{
+	return str[i];
+}
+
+int String::HowMany()
+{
+	return mun_strings;;
+}
+
+bool operator<(const String & st1, const String & st2)
+{
+	return (std::strcmp(st1.str, st2.str));
+}
+
+bool operator>(const String & st1, const String & st2)
+{
+	return st2 < st1;
+}
+
+bool operator==(const String & st1, const String & st2)
+{
+	return (std::strcmp(st1.str, st2.str) == 0);
+}
+
+
+
+std::ostream & operator <<(std::ostream & os, const String &st)          //重载 << 运算符
 {
 	os << st.str;
-	return os; 
+	return os;
+}
+
+std::istream & operator>>(std::istream & is, String & st)                //重载 >> 运算符
+{
+	char temp[String::CINLIM];
+	is.get(temp, String::CINLIM);
+	if (is)
+		st = temp;
+	while (is && is.get() != '\n')
+		continue;
+	return is;                                                           //可以完成连续操作
 }
